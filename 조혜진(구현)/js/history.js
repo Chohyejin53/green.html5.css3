152fee743d9712474aa33fa0f724bde978174dc2
Chohyejin53 <hyejin2338@gmail.com>
2021-04-26 18:19:09 +0900

    210426 수업 포폴

--- 조혜진(구현)/js/history.js
+++ 조혜진(구현)/js/history.js
@@ -3,45 +3,44 @@ $("document").ready(function(){
     var options = {
         videoId : 'NfLTCEfxEFE',
         start: 6,
-        wrapperZIndex: 99,
     };
     $("#wrapper").tubular(options);
 });
 
 
-// 슬라이드
+// // 슬라이드
 
-$(document).ready(function(){
-    var boxs;
-    var box_count;
-    var box_position = 1;
+// $(document).ready(function(){
+//     var boxs;
+//     var box_count;
+//     var box_position = 1;
 
-    boxs = $(".slide .slide_box");
-    box_count = boxs.children().length;
+//     boxs = $(".slide .slide_box");
+//     box_count = boxs.children().length;
 
-    //버튼을 클릭했을 때 함수 실행 
+//     //버튼을 클릭했을 때 함수 실행 
 
-    $('#back').click(function(){
-        next();
-    });
-    $('#next').click(function(){
-        back();
-    });
+//     $('#back').click(function(){
+//         next();
+//     });
+//     $('#next').click(function(){
+//         back();
+//     });
 
-    function back() {
-        if(1 <  box_position) {
-            boxs.animate ({
-                left:'+=1024px'
-            });
-            box_position--;
-        }
-    }
-    function next() {
-        if(box_count > box_position) {
-            boxs.animate ({
-                left:'-=1024px'
-            });
-            box_position++;
-        }
-    }
-});
+//     function back() {
+//         if(1 <  box_position) {
+//             boxs.animate ({
+//                 left:'+=1024px'
+//             });
+//             box_position--;
+//         }
+//     }
+//     function next() {
+//         if(box_count > box_position) {
+//             boxs.animate ({
+//                 left:'-=1024px'
+//             });
+//             box_position++;
+//         }
+//     }
+// });
