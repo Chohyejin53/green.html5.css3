fec5bb676239953a84e00f763662c4d7abfb885b
Chohyejin53 <hyejin2338@gmail.com>
2021-04-27 01:27:12 +0900

    210427 포폴

---
+++ 조혜진(구현)/js/object.js
@@ -0,0 +1,72 @@
+
+    //color 코드 넣기
+    // var arr = [];
+    // for(var i=0; i<obj.length; i++) {
+    //     arr[i] = obj[i].color;
+    // }
+
+    // obj.length를 이용해서 갯수 세기
+    // var colorData = [];
+    // for(var i=0; i<obj.length; i++) {
+    //     colorData[i] = 1;
+    // }
+
+    var data = { 
+        // labels: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"], 
+        datasets: [ { 
+            data: [1,1,1,1,1,1,1,1,1,1,1], 
+            backgroundColor: [ "#f79546", "#9bba57", "#4f81bb", "#5f497a", "#a94069", "#ff5f34", "#41774e", "#003663", "#49acc5", "#c0504e" ], 
+            // data: colorData, 
+            // backgroundColor: arr, 
+            borderWidth: 0, 
+            // label: "Dataset 1" 
+            }
+        ] 
+    };
+    
+    var config = {
+        type: 'pie',
+        data: data,
+        options: {
+            responsive: false,
+            plugins: {
+                legend: false,
+                title: {
+                    display: false,
+                    text: 'Chart.js Pie Chart'
+                },
+                tooltip: {
+                    enabled: false
+                }
+            },
+            onClick: chartClickEvent
+        },
+    };
+    
+    var myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
+    
+    /*
+     이벤트 참고- https://stackoverflow.com/questions/46672925/chart-js-onclick-event-with-a-mixed-chart-which-chart-did-i-click
+    */
+    function chartClickEvent(event, array){
+        console.log(array);
+        console.log("color hex code: " + array[0].element.options.backgroundColor);
+    }
+    
+    /* tooltip 제거(2번째 방법)*/
+    // Chart.defaults.plugins.tooltip.enabled = false;
+    
+
+
+    //img 분류 보여주기
+    $('.grid').isotope({
+        // options...
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+        masonry: {
+        columnWidth: 200
+        }
+    });
